e48d139a9ebd3a71334a4747fedce8df7cc19596
Casey <caclowers@gmail.com>
2019-01-21 22:48:01 -0600

    removed react-modal//will replace with alt modal

--- src/components/Projects/Projects.js
+++ src/components/Projects/Projects.js
@@ -1,6 +1,4 @@
 import React, { Component } from 'react';
-import ReactDOM from 'react-dom';
-import Modal from 'react-modal';
 import '../../App.css';
 
 //import images
@@ -8,16 +6,6 @@ import woodland1 from '../../images/Projects/woodland1.png';
 import potatohead1 from '../../images/Projects/potatohead1.png';
 import solcycle1 from '../../images/Projects/solcycle1.png';
 
-const customStyles = {
-  content : {
-    top                   : '50%',
-    left                  : '50%',
-    right                 : 'auto',
-    bottom                : 'auto',
-    marginRight           : '-50%',
-    transform             : 'translate(-50%, -50%)'
-  }
-};
 
 
 const project = [
@@ -37,40 +25,14 @@ const project = [
 ];
 
 class Projects extends Component {
-  constructor() {
-    super();
- 
-    this.state = {
-      modalIsOpen: false
-    };
- 
-    this.openModal = this.openModal.bind(this);
-    // this.afterOpenModal = this.afterOpenModal.bind(this);
-    this.closeModal = this.closeModal.bind(this);
-  }
- 
-  openModal() {
-    this.setState({modalIsOpen: true});
-  }
- 
-  // afterOpenModal() {
-  //   // references are now sync'd and can be accessed.
-  //   this.style.color = '#f00';
-  // }
- 
-  closeModal() {
-    this.setState({modalIsOpen: false});
-  }
+  
 
   render() {
     let projectArray = project.map((project, index) => {
       return (
-      <div className="projectImgDiv">
-        <div onClick={this.openModal}>
-          {project.name}
-        </div>
+      <div className="projectImgDiv" onClick={this.openModal}>
+        <div>{project.name}</div>
         <img 
-        onClick={this.openModal}
         className="projectImg"
         key={index}
         src={project.image}
@@ -82,19 +44,9 @@ class Projects extends Component {
     })
     return (
       <section id="about">
-        <h3>some projects i've worked on {/* <span style={{fontSize:10}}>(click for more info)</span>*/}</h3> 
-        
+        <h3>some projects i've worked on <span style={{fontSize:10}}>(hover for more info)</span></h3> 
         <div className="logoArray">
           {projectArray}
-
-          {/* <button onClick={this.openModal}>Open Modal</button> */}
-        <Modal
-          isOpen={this.state.modalIsOpen}
-          onAfterOpen={this.afterOpenModal}
-          onRequestClose={this.closeModal}
-          style={customStyles}
-          contentLabel="Example Modal"
-        ></Modal>
         </div>
       </section>
     )
